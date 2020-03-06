44802a5a6418f894f61235279b2d38157333e7fe
FOFE blondeau <b.fofe@yahoo.com>
2020-03-06 08:19:17 +0100

    Initial commit

---
+++ resources/views/layouts/_head.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>R | Nik </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset("template/img/favicon.ico") }}">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/bootstrap.min.css") }}">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/font-awesome.min.css") }}">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/owl.carousel.css") }}">
+    <link rel="stylesheet" href="{{ asset("template/css/owl.theme.css") }}">
+    <link rel="stylesheet" href="{{ asset("template/css/owl.transitions.css") }}">
+    <!-- meanmenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/meanmenu/meanmenu.min.css") }}">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/animate.css") }}">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/normalize.css") }}">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/scrollbar/jquery.mCustomScrollbar.min.css") }}">
+    <!-- jvectormap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/jvectormap/jquery-jvectormap-2.0.3.css") }}">
+    <!-- notika icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/notika-custom-icon.css") }}">
+    <!-- wave CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/wave/waves.min.css") }}">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/main.css") }}">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset("template/css/responsive.css") }}">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="{{ asset("template/js/vendor/modernizr-2.8.3.min.js") }}"></script>
+</head>
